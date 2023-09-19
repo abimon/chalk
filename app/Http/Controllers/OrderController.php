ffe068c4909bacbebc3eb5d00a12877039d897f6
abimon <eddyabby59@gmail.com>
2023-09-19 14:34:08 +0300

    Mpesa

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -105,15 +105,15 @@ class OrderController extends Controller
         $phone = request()->phone;
         $receipt = 'HLC' . date('YmdHs');
         foreach ($carts  as $cart) {
-            order::create([
-                'buyer_id' => $cart->buyer_id,
-                'product_id' => $cart->product_id,
-                'quantity' => $cart->quantity,
-                'pickup' => Auth()->user()->residence,
-                'more' => request()->more,
-                'receipt' => $receipt
-            ]);
-            cart::destroy($cart->id);
+            // order::create([
+            //     'buyer_id' => $cart->buyer_id,
+            //     'product_id' => $cart->product_id,
+            //     'quantity' => $cart->quantity,
+            //     'pickup' => Auth()->user()->residence,
+            //     'more' => request()->more,
+            //     'receipt' => $receipt
+            // ]);
+            // cart::destroy($cart->id);
             $total += ($cart->price) * ($cart->quantity);
         }
         // $this->stkpush($phone,$total,$receipt);
@@ -121,20 +121,19 @@ class OrderController extends Controller
         $originalStr = $phone;
         $prefix = substr($originalStr, 0, 1);
         $contact = str_replace('0', $code, $prefix) . substr($originalStr, 1);
-        $url = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+        $url = (env('MPESA_ENV')=='live')?'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest':'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $this->generate_token()));
         $curl_post_data = [
-            //Fill in the request parameters with valid values
             'BusinessShortCode' => env('MPESA_SHORT_CODE'),
             'Password' => $this->lipaNaMpesaPassword(),
             'Timestamp' => date('YmdHis'),
             'TransactionType' => 'CustomerPayBillOnline',
             'Amount' => $total,
-            'PartyA' => $contact, // replace this with your phone number
+            'PartyA' => $contact,
             'PartyB' => env('MPESA_SHORT_CODE'),
-            'PhoneNumber' => $contact, // replace this with your phone number
+            'PhoneNumber' => $contact,
             'CallBackURL' => 'https://chalkorganic.apekinc.top//api/v1/callback/' . $receipt,
             'AccountReference' => 'Receipt ' . $receipt,
             'TransactionDesc' => $receipt
@@ -147,11 +146,12 @@ class OrderController extends Controller
         $res = json_decode($curl_response);
         return $res;
         if ($res->ResponseCode == 0) {
-            return response()->json('Success', 200);
+            dd($res);
+            // return redirect('/products');
         } else {
             return response()->json('Something wrong happened. Try  again.', 400);
         }
-        return redirect('/products');
+        
     }
     function updateOrder($id)
     {
