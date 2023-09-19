92e70fa986b5185f473c868dc2db46e8b2c96d93
abimon <eddyabby59@gmail.com>
2023-09-19 10:42:54 +0300

    Mpesa

---
+++ database/migrations/2023_09_19_072116_create_mpesas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mpesas', function (Blueprint $table) {
+            $table->id();
+            $table->string('TransactionType');
+            $table->string('Receipt');
+            $table->string('MpesaReceiptNumber');
+            $table->string('TransactionDate');
+            $table->string('TransAmount');
+            $table->string('PhoneNumber');
+            $table->longText('response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mpesas');
+    }
+};
